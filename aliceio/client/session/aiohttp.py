2428768f35ff6462684e7075442d706ab65b2517
K1rLes <kirill.lesovoi@mail.ru>
2024-09-05 02:20:20 +0300

    Изменения из aiogram'а
    https://github.com/aiogram/aiogram/pull/1507
    https://github.com/aiogram/aiogram/pull/1514
    https://github.com/aiogram/aiogram/pull/1548
    https://github.com/aiogram/aiogram/pull/1571

--- aliceio/client/session/aiohttp.py
+++ aliceio/client/session/aiohttp.py
@@ -85,13 +85,26 @@ def _prepare_connector(
 
 
 class AiohttpSession(BaseSession):
-    def __init__(self, proxy: Optional[_ProxyType] = None, **kwargs: Any) -> None:
+    def __init__(
+        self,
+        proxy: Optional[_ProxyType] = None,
+        limit: int = 100,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Client session based on aiohttp.
+        :param proxy: The proxy to be used for requests. Default is None.
+        :param limit: The total number of simultaneous connections. Default is 100.
+        :param kwargs: Additional keyword arguments.
+        """
         super().__init__(**kwargs)
 
         self._session: Optional[ClientSession] = None
         self._connector_type: Type[TCPConnector] = TCPConnector
         self._connector_init: Dict[str, Any] = {
             "ssl": ssl.create_default_context(cafile=certifi.where()),
+            "limit": limit,
+            "ttl_dns_cache": 3600,  # https://github.com/aiogram/aiogram/issues/1500
         }
         self._should_reset_connector = True  # флаг определяет состояние коннектора
         self._proxy: Optional[_ProxyType] = None
