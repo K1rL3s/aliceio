781e53a39ecd583e429b0c8fa4606983c7cc96c9
K1rLes <kirill.lesovoi@mail.ru>
2024-12-23 05:22:30 +0300

    Тесты на prepare_value

--- aliceio/utils/funcs.py
+++ aliceio/utils/funcs.py
@@ -1,34 +1,50 @@
 import datetime
 import json
 from enum import Enum
-from typing import Any, Callable, Protocol
+from typing import Any, Callable, Literal, Protocol, Union
 
 from aiohttp import JsonPayload
 
 from aliceio.dispatcher.event.bases import REJECTED, UNHANDLED
 from aliceio.types import InputFile
+from aliceio.types.base import AliceObject
 
 
 class PrepareValue(Protocol):  # pragma: no cover
-    def __call__(self, value: Any, files: dict[str, Any]) -> Any: ...
+    def __call__(
+        self,
+        value: Any,
+        files: dict[str, Any],
+        _dumps_json: Union[Callable[..., str], Literal[False]] = False,
+    ) -> Any: ...
 
 
 # Ключи, у которых значение = None, не пропускаются, потому что иначе не получится
 # установить None в значение хранилища состояний API Алисы
-def prepare_value(value: Any, files: dict[str, Any]) -> Any:
+def prepare_value(
+    value: Any,
+    files: dict[str, Any],
+    _dumps_json: Union[Callable[..., str], Literal[False]] = False,
+) -> Any:
     """Подготовка значения перед отправкой."""
     if value in (None, UNHANDLED, REJECTED):
         return None
     if isinstance(value, str):
         return value
     if isinstance(value, InputFile):
-        key = "file"
+        key = "file"  # aiogram: key = secrets.token_urlsafe(10)
         files[key] = value
         return f"attach://{key}"
     if isinstance(value, dict):
-        return {key: prepare_value(item, files=files) for key, item in value.items()}
+        value = {key: prepare_value(item, files=files) for key, item in value.items()}
+        if _dumps_json:
+            return _dumps_json(value)
+        return value
     if isinstance(value, list):
-        return [prepare_value(item, files=files) for item in value]
+        value = [prepare_value(item, files=files) for item in value]
+        if _dumps_json:
+            return _dumps_json(value)
+        return value
     if isinstance(value, datetime.timedelta):
         now = datetime.datetime.now()
         return str(round((now + value).timestamp()))
@@ -36,15 +52,27 @@ def prepare_value(value: Any, files: dict[str, Any]) -> Any:
         return str(round(value.timestamp()))
     if isinstance(value, Enum):
         return prepare_value(value.value, files=files)
+    if isinstance(value, AliceObject):
+        return prepare_value(
+            value.model_dump(warnings=False),
+            files=files,
+            _dumps_json=_dumps_json,
+        )
+    if _dumps_json:
+        return _dumps_json(value)
     return value
 
 
+# TODO: В prepavre_value_fn не передаётся json_dumps, пофиксить?
 def build_json_payload(
     value: Any,
     prepare_value_fn: PrepareValue = prepare_value,
     json_dumps: Callable[..., str] = json.dumps,
 ) -> JsonPayload:
     return JsonPayload(
-        value=prepare_value_fn(value=value, files={}),
+        value=prepare_value_fn(
+            value=value,
+            files={},
+        ),
         dumps=json_dumps,
     )
